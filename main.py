094631891a70b541dcd2df1c4ded14af48ec1a87
tarasenkofedor <tarasenkafiodar@gmail.com>
2023-03-12 19:41:23 +0300

    Added function, which does entered operation to two entered numbers

--- main.py
+++ main.py
@@ -3,6 +3,21 @@ def print_hello_world():
     print("Hello, World!")
 
 
+# Gets two numbers and operation to apply to those numbers,
+# then does that operation and returns the result
+def calculate(first_number, second_number, operation):
+
+    if operation == "add":
+        return first_number + second_number
+    elif operation == "sub":
+        return first_number - second_number
+    elif operation == "mult":
+        return first_number * second_number
+    else:
+        return first_number / second_number
+
+
 # Runs the program
 if __name__ == '__main__':
     print_hello_world()
+    calculate(1, 2, "sum")
