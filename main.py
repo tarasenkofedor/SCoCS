0ff3c5da7fdc2994df2f7432ea534f51587c4ff9
tarasenkofedor <tarasenkafiodar@gmail.com>
2023-03-12 21:38:54 +0300

    Added functin, which generates list of numbers and returns the list of even numbers from first list

--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+# Imports
+from random import randint
+
+
 # Prints "Hello, World!"
 def print_hello_world():
     print("Hello, World!")
@@ -17,7 +21,13 @@ def calculate(first_number, second_number, operation):
         return first_number / second_number
 
 
+# Generates list of numbers and returns the list of even numbers from first list
+def get_even_numbers():
+    return [n for n in range(1, randint(5, 20)) if n % 2 == 0]
+
+
 # Runs the program
 if __name__ == '__main__':
     print_hello_world()
     calculate(1, 2, "sum")
+    get_even_numbers()
